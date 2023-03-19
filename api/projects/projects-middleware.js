0d3760460240dbe888e5d214bc31fd8b9e70e66e
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-19 15:26:46 -0400

    build project ID validation middleware and second endpoint (get project by id)

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,14 @@
-// add middlewares here related to projects
+const Projects = require("./projects-model");
+
+async function validateProjectID(req, res, next) {
+    req.project = await Projects.get(req.params.id);
+    if(req.project) next();
+    else next({
+        message: "No such project ID.",
+        status: 404
+    })
+}
+
+module.exports = {
+    validateProjectID
+};
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -3,7 +3,7 @@ const express = require("express");
 const Projects = require("./projects-model");
 
 const {
-
+    validateProjectID
 } = require("./projects-middleware");
 
 const router = express.Router();
@@ -11,6 +11,12 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
     Projects.get()
         .then( projects => res.json(projects))
+        .catch(next);
+})
+
+router.get("/:id", validateProjectID, (req, res, next) => {
+    Projects.get(req.params.id)
+        .then( project => res.json(project))
         .catch(next);
 })
 
