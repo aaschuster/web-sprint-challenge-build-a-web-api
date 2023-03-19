2e05564bd97aef256474d38dbb314607afb2d887
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-19 15:17:56 -0400

    wire up projects router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,15 @@
-// Write your "projects" router here!
+const express = require("express");
+
+const Projects = require("./projects-model");
+
+const {
+
+} = require("./projects-middleware");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json("Hello from projects router");
+})
+
+module.exports = router;
