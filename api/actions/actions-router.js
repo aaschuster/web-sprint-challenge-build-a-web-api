af8c57aa9797d59ef4e414093ad6f7aabb63591b
Aaron Schuster <aaronjschuster@gmail.com>
2023-03-19 16:22:06 -0400

    wire up actions router and build first endpoint for getting all actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,17 @@
-// Write your "actions" router here!
+const express = require("express")
+
+const Actions = require("./actions-model");
+
+// const {
+
+// } = require("./actions-middleware");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+    Actions.get()
+        .then( actions => res.json(actions) )
+        .catch(next);
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const projectsRouter = require("./projects/projects-router");
 const actionsRouter = require("./actions/actions-router");
 
 server.use("/api/projects", projectsRouter);
-// server.use("/api/actions", actionsRouter);
+server.use("/api/actions", actionsRouter);
 
 server.use( (err, req, res, next) => {
     res.status(err.status || 500).json({
